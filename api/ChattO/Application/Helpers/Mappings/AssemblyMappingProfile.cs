9ed80bf96b68f1472b890bfe2df9b9c68630947f
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-03-13 09:16:54 +0200

    Added create organization functionality

---
+++ api/ChattO/Application/Helpers/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Application.Helpers.Mappings;
+
+public class AssemblyMappingProfile : Profile
+{
+    public AssemblyMappingProfile(Assembly assembly)
+        => ApplyMappingsFromAssembly(assembly);
+
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetExportedTypes()
+            .Where(t => t.GetInterfaces()
+                           .Any(i => i.IsGenericType 
+                           && i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+            .ToList();
+    }
+}
---
+++ api/ChattO/Application/Organizations/Commands/CreateOrganization.cs
@@ -0,0 +1,54 @@
+﻿using Application.Interfaces;
+using FluentValidation;
+using Domain.Models;
+using MediatR;
+using Application.Helpers;
+
+namespace Application.Organizations.Commands;
+
+public class CreateOrganization
+{
+    public class  Command : IRequest<Result<Guid>>
+    {
+        public string Name { get; set; }
+        public string Domain { get; set; }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Domain).NotEmpty();
+        }
+    }
+    public class Handler : IRequestHandler<Command, Result<Guid>>
+    {
+        private readonly IChattoDbContext _dbContext;
+        public Handler(IChattoDbContext dbContext) => 
+            _dbContext = dbContext;
+        public async Task<Result<Guid>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var organization = new Organization
+            {
+                Name = request.Name,
+                Domain = request.Domain
+            };
+
+            try
+            {
+                await _dbContext.Organizations.AddAsync(organization);
+                var result = await _dbContext.SaveChangesAsync(cancellationToken) > 0;
+
+                if (!result)
+                    return Result.Failure<Guid>("Failed to create organization");
+
+                return Result.Success(organization.Id);
+            }
+            catch
+            {
+                return Result.Failure<Guid>("Failed to save organization");
+            }
+        }
+    }
+}
