519fb9b7a969b57cae202e747e943c38bbb75a2c
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-03-16 14:17:05 +0200

    Billings models and migration

---
+++ api/ChattO/Domain/Models/Billing.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models;
+
+public class Billing
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid OrganizationId { get; set; }
+    public virtual Organization Organization { get; set; }
+    public Guid BillingInfoId { get; set; }
+    public virtual BillingInfo BillingInfo { get; set; }
+}
---
+++ api/ChattO/Domain/Models/BillingInfo.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Domain.Models;
+
+public class BillingInfo
+{
+    public Guid Id { get; set; }
+    public BillingType Type { get; set; }
+    public double Price { get; set; }
+    public virtual ICollection<Billing> Billings { get; set; }
+}
