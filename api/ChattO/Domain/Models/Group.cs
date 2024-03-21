f1f13704fb4ef3f0f09e8e1a4ec79b998520824f
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-03-21 21:17:34 +0200

    Group-organization migration

--- api/ChattO/Domain/Models/Group.cs
+++ api/ChattO/Domain/Models/Group.cs
@@ -4,6 +4,8 @@ public class Group
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public Guid OrganizationId { get; set; }
+    public virtual Organization Organization { get; set; }
     public virtual ICollection<AppUserGroup> AppUserGroups { get; set; }
     public virtual ICollection<Feed> Feeds { get; set; }
 }
