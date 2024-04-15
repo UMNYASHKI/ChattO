edcc9b8ddd98e59b97ef0610e1f75c8218d39758
bieloshapka <mykyta.bieloshapka@nixs.com>
2024-04-15 17:10:20 +0300

    create async api specification manager

---
+++ api/ChattO/API/Helpers/AsyncApiSpecificationManager.cs
@@ -0,0 +1,17 @@
+﻿using LEGO.AsyncAPI.Models;
+using System.Reflection;
+using System.Text.Json;
+
+namespace API.Helpers;
+
+public static class AsyncApiSpecificationManager
+{
+    public static async void UpdateSpecification()
+    {
+        //using var stream = new FileStream("async-api.yml", FileMode.OpenOrCreate);
+
+        //var document = new AsyncApiDocument();
+
+        //await JsonSerializer.SerializeAsync(stream, document);
+    }
+}
