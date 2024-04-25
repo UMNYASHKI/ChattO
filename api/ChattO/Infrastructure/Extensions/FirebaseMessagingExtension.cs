3d7521a800c8edcfeafbfaa5ba6bda8766ccbf05
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-04-25 13:06:00 +0300

    environment variable setted for Firbase configuration

--- api/ChattO/Infrastructure/Extensions/FirebaseMessagingExtension.cs
+++ api/ChattO/Infrastructure/Extensions/FirebaseMessagingExtension.cs
@@ -8,9 +8,13 @@ public static class FirebaseMessagingExtension
 {
     public static void AddFirbaseApp(this IApplicationBuilder builder)
     {
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS",
+        Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+        "chatto-419213-firebase-adminsdk-xq7rm-ca2456e87b.json"));
+
         FirebaseApp.Create(new AppOptions()
         {
-            Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "chatto-419213-firebase-adminsdk-xq7rm-ca2456e87b.json")),
+            Credential = GoogleCredential.GetApplicationDefault(),
         });
     }
 }
