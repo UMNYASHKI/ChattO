4e3ce4c4785587b1383a9932b441acb0d08e838f
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-05-16 20:16:15 +0300

    Set up Mailjet dependencies
    Installed the nuget package. Configured dependency injection for the EmailService. Added key sections in appsetings and docker-compose

---
+++ api/ChattO/Infrastructure/Services/EmailSending/EmailService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Abstractions;
+using Mailjet.Client;
+
+namespace Infrastructure.Services.EmailSending;
+
+public class EmailService : IEmailService 
+{
+    private readonly IMailjetClient _mailjetClient;
+    public EmailService(IMailjetClient mailjetClient)
+    {
+        _mailjetClient = mailjetClient;
+    }
+}
