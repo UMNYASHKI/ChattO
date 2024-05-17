ea8274156a7b3e639f33a00f51ade53a238ea497
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-05-17 11:07:33 +0300

    Create model for user email data

---
+++ api/ChattO/Application/DTOs/EmailData/UserRegistration.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs.EmailData;
+
+public record UserRegistration(string DisplayName,
+    string Email,
+    AppUserRole Role,
+    string GenaratedPassword);
