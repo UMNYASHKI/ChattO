a66a50c7aa2b4193554b0a4171ac86c3895ac1f5
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-05-21 15:22:46 +0300

    Add model for paging properties in Application

---
+++ api/ChattO/Application/Helpers/PagingProps.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Helpers;
+
+public class PagingProps
+{
+    public string? ColumnName { get; set; } = "Id";
+    public bool? Descending { get; set; } = true;
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
