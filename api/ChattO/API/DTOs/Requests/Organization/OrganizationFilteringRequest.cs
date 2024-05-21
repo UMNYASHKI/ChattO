fa3f8b02b8b01f5a2c3f30d86435bdb1d75cf13e
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2024-05-21 16:00:08 +0300

    Add get organizations endpoint

--- api/ChattO/API/DTOs/Requests/Organization/OrganizationFilteringRequest.cs
+++ api/ChattO/API/DTOs/Requests/Organization/OrganizationFilteringRequest.cs
@@ -1,9 +1,23 @@
 ﻿using API.DTOs.Sorting;
+using Application.Helpers.Mappings;
+using Application.Organizations.Queries;
+using AutoMapper;
 
 namespace API.DTOs.Requests.Organization;
 
-public class OrganizationFilteringRequest : SortingParams
+public class OrganizationFilteringRequest : SortingParams, IMapWith<GetListOrganizations.Query>
 {
     public string? Name { get; set; }
     public string? Domain { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<OrganizationFilteringRequest, GetListOrganizations.Query>()
+            .ForMember(q => q.Name, opt => opt.MapFrom(r => r.Name))
+            .ForMember(q => q.Domain, opt => opt.MapFrom(r => r.Domain))
+            .ForMember(q => q.ColumnName, opt => opt.MapFrom(r => r.ColumnName))
+            .ForMember(q => q.Descending, opt => opt.MapFrom(r => r.Descending))
+            .ForMember(q => q.PageNumber, opt => opt.MapFrom(r => r.PageNumber))
+            .ForMember(q => q.PageSize, opt => opt.MapFrom(r => r.PageSize));
+    }
 }
---
+++ api/ChattO/Application/Organizations/Queries/GetListOrganizations.cs
@@ -0,0 +1,50 @@
+﻿using Application.Abstractions;
+using Application.Helpers;
+using Domain.Models;
+using FluentValidation;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Organizations.Queries;
+
+public class GetListOrganizations
+{
+    public class Query : PagingProps, IRequest<Result<PagingResponse<Organization>>>
+    {
+        [Filter]
+        public string? Name { get; set; }
+        [Filter]
+        public string? Domain { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Result<PagingResponse<Organization>>>
+    {
+        private readonly IRepository<Organization> _repository;
+        private readonly IValidator<PagingProps> _validator;
+        public Handler(IRepository<Organization> repository, IValidator<PagingProps> validator)
+        {
+            _repository = repository;
+            _validator = validator;
+        }
+        public async Task<Result<PagingResponse<Organization>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+            if (!validationResult.IsValid)
+                return Result.Failure<PagingResponse<Organization>>(validationResult.ToString(" "));
+
+            var filter = ExpressionFilter<Organization, Query>.GetFilter(request);
+            var sortBy = SortingBuilder<Organization>.GetSortBy(request.ColumnName, request.Descending ?? true);
+
+            var listResult = await _repository.GetAllAsync(filter, sortBy, request.PageNumber, request.PageSize);
+            if(!listResult.IsSuccessful)
+                return Result.Failure<PagingResponse<Organization>>(listResult.Message);
+            
+            var totalAmountResult = await _repository.GetTotalCountAsync(filter);
+            if(!totalAmountResult.IsSuccessful)
+                return Result.Failure<PagingResponse<Organization>>(totalAmountResult.Message);
+
+            return Result.Success(new PagingResponse<Organization>(listResult.Data, totalAmountResult.Data,
+                request.PageNumber, request.PageSize));
+        }
+    }
+}
