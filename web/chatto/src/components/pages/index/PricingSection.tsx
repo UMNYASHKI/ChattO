6d36eab83f38ad097bc607f1d38df2bdde82cf83
yuliia druchek <yuliia.druchek@nure.ua>
2024-06-02 14:05:10 +0200

    Update PricingSection.tsx

--- web/chatto/src/components/pages/index/PricingSection.tsx
+++ web/chatto/src/components/pages/index/PricingSection.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
 
+import PriceCard from '@/components/common/price-card';
+
 const PricingSection: React.FC = () => {
-    return (
-        <section className="flex flex-col items-center p-10">
-            <h2 className="text-2xl font-bold mb-4">Our Pricing</h2>
-            <div className="flex space-x-10">
-                <div className="bg-gray-100 p-6 rounded-lg shadow-md text-center">
-                    <h3 className="text-xl font-bold">3 Month</h3>
-                    <p className="text-2xl font-bold my-4">$100</p>
-                    <p>per organization</p>
-                    <button className="mt-4 px-4 py-2 bg-black text-white rounded">
-                        Get started
-                    </button>
-                </div>
-                <div className="bg-gray-100 p-6 rounded-lg shadow-md text-center">
-                    <h3 className="text-xl font-bold">6 Months</h3>
-                    <p className="text-2xl font-bold my-4">$190</p>
-                    <p>per organization</p>
-                    <button className="mt-4 px-4 py-2 bg-black text-white rounded">
-                        Get started
-                    </button>
-                </div>
-                <div className="bg-gray-100 p-6 rounded-lg shadow-md text-center">
-                    <h3 className="text-xl font-bold">1 Year</h3>
-                    <p className="text-2xl font-bold my-4">$360</p>
-                    <p>per organization</p>
-                    <button className="mt-4 px-4 py-2 bg-black text-white rounded">
-                        Get started
-                    </button>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="flex flex-col items-center py-20 w-full space-y-20">
+      <h2 className="text-4xl font-bold mb-4">Our Pricing</h2>
+      <div className="flex justify-center gap-8 w-full">
+        <PriceCard period="3 MONTH" price={100} className="basis-1/3" />
+        <PriceCard period="6 MONTH" price={190} className="basis-1/3" />
+        <PriceCard period="1 YEAR" price={360} className="basis-1/3" />
+      </div>
+    </section>
+  );
 };
 
 export default PricingSection;
