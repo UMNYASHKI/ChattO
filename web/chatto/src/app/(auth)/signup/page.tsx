12eee081eee50e18d053731636f22ed08f999856
KaterynaDerkach <katederkach2005@gmail.com>
2024-06-02 14:30:08 +0300

    Update page.tsx

--- web/chatto/src/app/(auth)/signup/page.tsx
+++ web/chatto/src/app/(auth)/signup/page.tsx
@@ -1,5 +1,23 @@
-import { SignupForm } from '@/components/common/auth/SignupForm';
+import { useState } from 'react';
 
-export default function SignUpRoute() {
-  return <SignupForm />;
-}
+import { RegistrationStep1Form } from '@/components/pages/signup/step1';
+import { RegistrationStep2Form } from '@/components/pages/signup/step2';
+
+const Registration: React.FC = () => {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({});
+
+  const handleNext = (data) => {
+    setFormData({ ...formData, ...data });
+    setStep(step + 1);
+  };
+
+  return (
+    <main className="flex flex-1 w-full items-center justify-center">
+      {step === 1 && <RegistrationStep1Form onNext={handleNext} />}
+      {step === 2 && <RegistrationStep2Form onNext={handleNext} />}
+    </main>
+  );
+};
+
+export default Registration;
