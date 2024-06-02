50e7eec1df27b21ebd20d4d0f673e67c635253d0
yuliia druchek <yuliia.druchek@nure.ua>
2024-06-02 14:06:26 +0200

    Update page.tsx

--- web/chatto/src/app/page.tsx
+++ web/chatto/src/app/page.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 
 import Footer from '@/components/common/footer';
+import Header from '@/components/pages/index/Header';
 
 import DashboardSection from '../components/pages/index/DashboardSection';
 import FaqSection from '../components/pages/index/FaqSection';
 import MobileSection from '../components/pages/index/MobileSection';
 import PricingSection from '../components/pages/index/PricingSection';
 import StartSection from '../components/pages/index/StartSection';
-import Layout from './layout';
 
 const LandingPage: React.FC = () => {
-    return (
-        <Layout>
-            <StartSection />
-            <DashboardSection />
-            <MobileSection />
-            <PricingSection />
-            <FaqSection />
-            <Footer />
-        </Layout>
-    );
+  return (
+    <>
+      <Header />
+      <main className="px-64">
+        <StartSection />
+        <DashboardSection />
+        <MobileSection />
+        <PricingSection />
+        <FaqSection />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default LandingPage;
