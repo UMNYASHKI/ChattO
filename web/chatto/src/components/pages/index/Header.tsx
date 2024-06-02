efd444f98246bef6f9373b4744bd6d1e27ccc93c
yuliia druchek <yuliia.druchek@nure.ua>
2024-06-02 14:04:21 +0200

    Update Header.tsx

--- web/chatto/src/components/pages/index/Header.tsx
+++ web/chatto/src/components/pages/index/Header.tsx
@@ -1,31 +1,24 @@
+import Link from 'next/link';
 import React from 'react';
 
+import Logo from '@/components/common/logo';
+
 const Header: React.FC = () => {
-    return (
-        <header className="flex justify-between items-center p-6">
-            <div className="flex items-center">
-                <img src="/logo.png" alt="ChattO" className="h-8 w-8" />
-                <span className="ml-2 text-xl font-bold">ChattO</span>
-            </div>
-            <nav className="flex space-x-6">
-                <a href="#about" className="text-gray-700">
-                    About us
-                </a>
-                <a href="#download" className="text-gray-700">
-                    Download app
-                </a>
-                <a href="#pricing" className="text-gray-700">
-                    Pricing
-                </a>
-                <a href="#faq" className="text-gray-700">
-                    FAQ
-                </a>
-                <a href="#login" className="text-gray-700">
-                    Log in
-                </a>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="w-full flex justify-between items-center p-6 text-xl">
+      <Link href="/" className="flex items-center space-x-4">
+        <Logo className="h-8 w-8 fill-black" />
+        <span>ChattO</span>
+      </Link>
+      <nav className="flex space-x-6 ">
+        <a href="/#about">About us</a>
+        <a href="/#download">Download app</a>
+        <a href="/#pricing">Pricing</a>
+        <a href="/#faq">FAQ</a>
+      </nav>
+      <Link href="/signin">Log in</Link>
+    </header>
+  );
 };
 
 export default Header;
