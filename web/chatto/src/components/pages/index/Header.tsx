e8285d4296a08fbe9ea34aa8a17dadde18681b6d
Bilous Mykola <nick20031103@gmail.com>
2024-06-04 07:15:24 +0300

    Update Header.tsx

--- web/chatto/src/components/pages/index/Header.tsx
+++ web/chatto/src/components/pages/index/Header.tsx
@@ -4,21 +4,21 @@ import React from 'react';
 import Logo from '@/components/common/logo';
 
 const Header: React.FC = () => {
-  return (
-    <header className="w-full flex justify-between items-center p-6 text-xl">
-      <Link href="/" className="flex items-center space-x-4">
-        <Logo className="h-8 w-8 fill-black" />
-        <span>ChattO</span>
-      </Link>
-      <nav className="flex space-x-6 ">
-        <a href="/#about">About us</a>
-        <a href="/#download">Download app</a>
-        <a href="/#pricing">Pricing</a>
-        <a href="/#faq">FAQ</a>
-      </nav>
-      <Link href="/signin">Log in</Link>
-    </header>
-  );
+	return (
+		<header className="w-full flex justify-between items-center p-6 text-xl">
+			<Link href="/" className="flex items-center space-x-4">
+				<Logo className="h-8 w-8 fill-black" />
+				<span>ChattO</span>
+			</Link>
+			<nav className="flex space-x-6 ">
+				<a href="/#about">About us</a>
+				<a href="/#download">Download app</a>
+				<a href="/#pricing">Pricing</a>
+				<a href="/#faq">FAQ</a>
+			</nav>
+			<Link href="/signin">Log in</Link>
+		</header>
+	);
 };
 
 export default Header;
