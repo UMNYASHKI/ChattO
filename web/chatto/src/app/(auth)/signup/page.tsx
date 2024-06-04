e26fe5541eb7e51ddc54eb6588da864a06a3846f
Bilous Mykola <nick20031103@gmail.com>
2024-06-04 07:18:05 +0300

    Update page.tsx

--- web/chatto/src/app/(auth)/signup/page.tsx
+++ web/chatto/src/app/(auth)/signup/page.tsx
@@ -1,23 +1,7 @@
-import { useState } from 'react';
+import { SignUpForm } from '@/components/pages/signup/form';
 
-import { RegistrationStep1Form } from '@/components/pages/signup/step1';
-import { RegistrationStep2Form } from '@/components/pages/signup/step2';
-
-const Registration: React.FC = () => {
-  const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState({});
-
-  const handleNext = (data) => {
-    setFormData({ ...formData, ...data });
-    setStep(step + 1);
-  };
-
-  return (
-    <main className="flex flex-1 w-full items-center justify-center">
-      {step === 1 && <RegistrationStep1Form onNext={handleNext} />}
-      {step === 2 && <RegistrationStep2Form onNext={handleNext} />}
-    </main>
-  );
+const SignUpPage: React.FC = () => {
+	return <SignUpForm />;
 };
 
-export default Registration;
+export default SignUpPage;
