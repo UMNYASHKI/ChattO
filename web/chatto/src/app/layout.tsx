f69416b02cd391126e3951c1873096d1aef64e26
Bilous Mykola <nick20031103@gmail.com>
2024-06-04 07:13:44 +0300

    Update layout.tsx

--- web/chatto/src/app/layout.tsx
+++ web/chatto/src/app/layout.tsx
@@ -5,22 +5,22 @@ import { Mitr } from 'next/font/google';
 import React from 'react';
 
 const font = Mitr({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700'],
-  display: 'swap',
-  variable: '--font-mitr'
+	subsets: ['latin'],
+	weight: ['300', '400', '500', '700'],
+	display: 'swap',
+	variable: '--font-mitr'
 });
 
 export default function RootLayout({
-  children
+	children
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html className="scroll-smooth">
-      <head>
-        <title>ChattO</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className={clsx(font.variable, 'font-mitr')}>{children}</body>
-    </html>
-  );
+	return (
+		<html className="scroll-smooth">
+			<head>
+				<title>ChattO</title>
+				<link rel="icon" href="/favicon.ico" />
+			</head>
+			<body className={clsx(font.variable, 'font-mitr')}>{children}</body>
+		</html>
+	);
 }
